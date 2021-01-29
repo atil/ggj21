fa38b575fbe83ccefc32626c2e0a877429fe3abc
eusekerci <enesugursekerci@gmail.com>
2021-01-30 00:19:09 +0100

    text show on both bottom and up

--- Assets/Scripts/TextShowOnCollision.cs
+++ Assets/Scripts/TextShowOnCollision.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 
 public class TextShowOnCollision : MonoBehaviour
 {
-    public string _text;
-    
+    public string Text;
+    public bool ShowOnUp;
+    public GameObject TextUpGo;
+    public GameObject TextBottomGo;
+
     private GameObject _textGo;
     private Text _textUI;
     private BoxCollider2D _collider;
@@ -16,12 +19,18 @@ public class TextShowOnCollision : MonoBehaviour
 
     void Start()
     {
-        _textGo = GameObject.Find("Textbox"); 
+        if (ShowOnUp)
+        {
+            _textGo = TextUpGo;    
+        }
+        else
+        {
+            _textGo = TextBottomGo;
+        }
+
         _textUI = _textGo.GetComponent<Text>();
         _collider = GetComponent<BoxCollider2D>();
         _onTrigger = false;
-
-        _textGo.SetActive(false);
     }
 
     private void Update()
@@ -45,7 +54,7 @@ public class TextShowOnCollision : MonoBehaviour
         if (!_onTrigger && _onTriggerCurrentFrame)
         {
             _textGo.SetActive(true);
-            _textUI.text = _text;
+            _textUI.text = Text;
         }
 
         if (_onTrigger && !_onTriggerCurrentFrame)
