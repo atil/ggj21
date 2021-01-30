0c68652e324ab5c02c6011ce735b9370e11c32d9
atil <hyperion91@gmail.com>
2021-01-30 02:19:40 +0100

    Traverse progress

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,6 +7,7 @@ public class Game : MonoBehaviour
     public Player Player;
 
     public Room CurrentRoom;
+    public GridLayout Grid;
 
     public Transform RoomTargetLeft;
     public Transform RoomTargetRight;
@@ -24,43 +25,93 @@ public class Game : MonoBehaviour
         switch (direction)
         {
             case TraverseDirection.Up:
+                StartCoroutine(TeleportPlayer(direction, CurrentRoom.RoomUp));
                 StartCoroutine(DoRoomTransition(CurrentRoom.RoomUp, RoomTargetDown, RoomTargetUp));
                 break;
             case TraverseDirection.Down:
+                StartCoroutine(TeleportPlayer(direction, CurrentRoom.RoomDown));
                 StartCoroutine(DoRoomTransition(CurrentRoom.RoomDown, RoomTargetUp, RoomTargetDown));
                 break;
             case TraverseDirection.Left:
+                StartCoroutine(TeleportPlayer(direction, CurrentRoom.RoomLeft));
                 StartCoroutine(DoRoomTransition(CurrentRoom.RoomLeft, RoomTargetRight, RoomTargetLeft));
                 break;
             case TraverseDirection.Right:
+                StartCoroutine(TeleportPlayer(direction, CurrentRoom.RoomRight));
                 StartCoroutine(DoRoomTransition(CurrentRoom.RoomRight, RoomTargetLeft, RoomTargetRight));
                 break;
             default:
                 Debug.LogError($"Undefined traverse direction {direction}");
                 break;
         }
-
     }
 
-    private IEnumerator DoRoomTransition(Room targetRoom, Transform currentRoomTarget, Transform toRoomTarget)
+    private IEnumerator TeleportPlayer(TraverseDirection direction, Room targetRoom)
+    {
+        Player.transform.SetParent(CurrentRoom.transform);
+        yield return new WaitForSeconds(TraverseDuration / 2f);
+        Vector3Int cellPos = Grid.WorldToCell(Player.transform.position);
+        switch (direction)
+        {
+            case TraverseDirection.Up:
+                cellPos = new Vector3Int(cellPos.x, -5, 0);
+                break;
+            case TraverseDirection.Down:
+                cellPos = new Vector3Int(cellPos.x, 5, 0);
+                break;
+            case TraverseDirection.Left:
+                cellPos = new Vector3Int(5, cellPos.y, 0);
+                break;
+            case TraverseDirection.Right:
+                cellPos = new Vector3Int(-5, cellPos.y, 0);
+                break;
+        }
+
+        Player.transform.localPosition = Grid.CellToWorld(cellPos) + new Vector3(-0.5f, 0.5f, 0);
+        Player.transform.SetParent(targetRoom.transform, false);
+        yield return new WaitForSeconds(TraverseDuration / 2f);
+        Player.transform.SetParent(null);
+    }
+
+    private IEnumerator DoRoomTransition(Room targetRoom, Transform currentRoomTarget, Transform destinationRoomTarget)
     {
         IsTraversing = true;
         if (targetRoom != null)
         {
             StartCoroutine(RoomMoveCoroutine(CurrentRoom, RoomTargetMiddle, currentRoomTarget, true, false));
-            StartCoroutine(RoomMoveCoroutine(targetRoom, toRoomTarget, RoomTargetMiddle, true, true));
+            StartCoroutine(RoomMoveCoroutine(targetRoom, destinationRoomTarget, RoomTargetMiddle, true, true));
+            StartCoroutine(CoverCoroutine(targetRoom));
             CurrentRoom = targetRoom;
         }
         else
         {
             Debug.LogError($"No target room defined for room {CurrentRoom}");
         }
-        Player.transform.SetParent(currentRoomTarget);
-        yield return new WaitForSeconds(TraverseDuration / 2f);
-        Player.transform.localPosition = Vector3.zero;
-        //Player.transform.SetParent();
-        yield return new WaitForSeconds(TraverseDuration / 2f);
+        yield return new WaitForSeconds(TraverseDuration);
         IsTraversing = false;
+    }
+
+    private IEnumerator CoverCoroutine(Room targetRoom)
+    {
+        SpriteRenderer currentCover = CurrentRoom.transform.Find("Cover").GetComponent<SpriteRenderer>();
+        SpriteRenderer targetCover = targetRoom.transform.Find("Cover").GetComponent<SpriteRenderer>();
+
+        StartCoroutine(DoCoverCoroutine(currentCover, true));
+        yield return new WaitForSeconds(TraverseDuration / 2f);
+        StartCoroutine(DoCoverCoroutine(targetCover, false));
+    }
+
+    private IEnumerator DoCoverCoroutine(SpriteRenderer cover, bool enableCover)
+    {
+        float srcAlpha = enableCover ? 0f : 1f;
+        float targetAlpha = enableCover ? 1f : 0f;
+        for (float f = 0; f < TraverseDuration; f += Time.deltaTime)
+        {
+            Color c = cover.color;
+            c.a = Mathf.Lerp(srcAlpha, targetAlpha, f / TraverseDuration);
+            cover.color = c;
+            yield return null;
+        }
     }
 
     private IEnumerator RoomMoveCoroutine(Room room, Transform moveSource, Transform moveTarget, bool activeBeforeMove, bool activeAfterMove)
