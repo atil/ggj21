6f02e33e50dc28f9ee971c00ef0d8566734e9317
eusekerci <enesugursekerci@gmail.com>
2021-01-31 04:33:36 +0100

    disabler and text changer

---
+++ Assets/Scripts/Events/ChangeTextAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTextAction : EventAction
+{
+	public TextShowOnCollision TargetTextTrigger;
+	public string NewText;
+
+	public override void Call(RoomEnteranceDirection direction, bool isFirstEntry)
+	{
+		TargetTextTrigger.Text = NewText;
+	}
+}
---
+++ Assets/Scripts/Events/DisableObjectAction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableObjectAction : EventAction
+{
+	public GameObject TargetObject;
+	public override void Call(RoomEnteranceDirection direction, bool isFirstEntry)
+	{
+		TargetObject.SetActive(false);
+	}
+}
