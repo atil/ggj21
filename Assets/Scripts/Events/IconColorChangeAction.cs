9dff6857dc0e40f872cb8ca553947e52d6d43679
eusekerci <enesugursekerci@gmail.com>
2021-01-31 01:57:48 +0100

    icon color change

---
+++ Assets/Scripts/Events/IconColorChangeAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconColorChangeAction : EventAction
+{
+    public SpriteRenderer Icon;
+
+    public Color NewIconColor;
+    
+    public override void Call(RoomEnteranceDirection direction, bool isFirstEntry)
+    {
+        Icon.color = NewIconColor;
+    }
+}
