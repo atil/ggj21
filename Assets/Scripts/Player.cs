4ed8cb97a32155f4bf40d82236356691b9937f2f
atil <hyperion91@gmail.com>
2021-02-04 00:28:47 +0100

    Fix (I think) breaking movement bug
    The problem was that we stop the coroutine on traversal, therefore _isMoving bool is never cleared to false, so we the thing always early returns

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -125,6 +125,7 @@ public class Player : MonoBehaviour
                     CanTraverse = false;
                     if (_moveCoroutine != null)
                     {
+                        _isMoving = false;
                         StopCoroutine(_moveCoroutine);
                     }
                     Game.Traverse(traverseTrigger.Direction);
